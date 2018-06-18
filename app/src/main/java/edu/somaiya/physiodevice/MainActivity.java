3381304e2cbda5fefb239d82804a81e5b9273bed
Vatsal Kanakiya <vatsal.kanakiya@somaiya.edu>
2018-06-19 02:04:27 +0530

    Login Page Works Updated Theme and created request from Login button using Volley
    Signed-off-by: Vatsal Kanakiya <vatsal.kanakiya@somaiya.edu>

--- app/src/main/java/edu/somaiya/physiodevice/MainActivity.java
+++ app/src/main/java/edu/somaiya/physiodevice/MainActivity.java
@@ -1,36 +1,64 @@
 package edu.somaiya.physiodevice;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity {
 
-    public static final String server_ip_address = "192.168.43.200:5000";
+    public static final String server_ip_address = "192.168.43.178:5000";
     public static Integer global_rep = 1;
     Button login;
-    EditText username, password, ip_address, rep_et;
+    EditText username, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        this.login = (Button) findViewById(R.id.login_button);
-        /*this.username = (EditText) findViewById(R.id.username_edit_text);
-        this.password =  (EditText) findViewById(R.id.password_edit_text);
-        this.ip_address =  (EditText) findViewById(R.id.ip_add_edit_text);*/
-        this.rep_et = (EditText) findViewById(R.id.rep_edit_text);
+        this.login = findViewById(R.id.login_button);
+        this.username = findViewById(R.id.username_edit_text);
+        this.password = findViewById(R.id.password_edit_text);
         login.setOnClickListener((view) -> {
-            /*String ip_text = ip_address.getText().toString().trim();
             String username_text = username.getText().toString().trim();
             String password_text = password.getText().toString().trim();
-            server_ip_address = ip_text; */
-            global_rep = Integer.parseInt(rep_et.getText().toString().trim());
+            RequestQueue requestQueue = Volley.newRequestQueue(this);
             Intent intent = new Intent(this, ProfilesActivity.class);
-            startActivity(intent);
+            StringRequest stringRequest = new StringRequest(Request.Method.POST, "http://" + server_ip_address + "/loginverify", new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    if (response.equals("OK")) {
+                        startActivity(intent);
+                    } else {
+                        Log.d("LOGINRESP", response);
+                    }
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    error.printStackTrace();
+                }
+            }) {
+                protected Map<String, String> getParams() {
+                    Map<String, String> MyData = new HashMap<String, String>();
+                    MyData.put("email", username_text); // Send email
+                    MyData.put("password", password_text); // Send email
+                    return MyData;
+                }
+            };
+            requestQueue.add(stringRequest);
         });
     }
 }
